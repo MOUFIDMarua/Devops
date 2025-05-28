a00c325e4b703384207cda896e74067e8924cc12
Ton Nom <ton@email.com>
2025-05-28 16:47:31 +0100

    update

--- index.php
+++ index.php
@@ -4,7 +4,9 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="icon" type="image/png" href="assets/img/logo.png" />
-    <title>Agriculture Portal</title>
+    <title>ANA SARA
+      
+    </title>
 
   <!--     Fonts and icons     -->
   
